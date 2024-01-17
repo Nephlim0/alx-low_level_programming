bf46ec8ef778d8d477878b451784d3b37cf3b85d
Nephlim0 <135203543+Nephlim0@users.noreply.github.com>
2024-01-18 00:58:11 +0200

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Compile each .c file into a .o file
 gcc -c *.c
+
+# Create the static library liball.a from all .o files
 ar rc liball.a *.o
+
+# Index the static library for faster symbol lookup
 ranlib liball.a
+
+# Clean up the intermediate .o files
+rm *.o
